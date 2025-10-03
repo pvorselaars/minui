898f3180d3465a86951cf43d238f2fffa8f8b715
Perry Vorselaars <p.vorselaars@gmail.com>
2025-10-03 13:33:19 +0200

    Pass url parameters to route components (#1)

--- router.ts
+++ router.ts
@@ -1,24 +1,38 @@
 let currentComponent: any = null;
 
-export function router(target: HTMLElement, routes: Record<string, () => any>) {
+export function router(target: HTMLElement, routes: Record<string, (inputs?: any) => any>) {
 
-  function render(path: string) {
+   function parseUrl(url: string) {
+    const [path, queryString] = url.split("?");
+    const params: Record<string, string> = {};
+    if (queryString) {
+      queryString.split("&").forEach(pair => {
+        const [key, value] = pair.split("=");
+        params[decodeURIComponent(key)] = decodeURIComponent(value ?? "");
+      });
+    }
+    return { path, params };
+  }
+
+  function render(url: string) {
+    const { path, params } = parseUrl(url);
+
     if (currentComponent) {
       target.innerHTML = "";
     }
 
     const factory = routes[path] || routes["/"];
     if (factory) {
-      currentComponent = factory();
+      currentComponent = factory({params});
       currentComponent.mount(target);
     }
   }
 
   window.addEventListener("popstate", () => {
-    render(window.location.pathname);
+    render(window.location.pathname + window.location.search);
   });
 
-  render(window.location.pathname);
+  render(window.location.pathname + window.location.search);
 }
 
 export function go(path: string) {
